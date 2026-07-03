e17a71a414aed92f66628a3f627d1cae96443a10
Fidel Salazar <fidelsalazar990923@gmail.com>
2026-07-03 12:05:32 -0400

    fix (root): change sitemap.xml

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export const dynamic = 'force-static';
 
 export default function robots() {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: { userAgent: '*', allow: '/', disallow: '' },
     sitemap: `${siteConfig.url}${siteConfig.basePath}/sitemap.xml`,
   };
 }
